b8dbda03f399af91b1a88541ee0098493b2a3baf
unknown <gomez.lautaro@outlook.com>
2023-01-24 00:28:28 -0300

    okoko

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === dsadsa;
+const nuevoModulo = 21 % 5 === dsads¿Sa;
 
 
 // En los próximos 22 problemas, deberás completar la función.
