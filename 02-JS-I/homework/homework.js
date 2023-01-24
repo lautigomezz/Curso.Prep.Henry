552c5da4036aad232fea970d22f8e761d55dfb21
unknown <gomez.lautaro@outlook.com>
2023-01-23 22:24:01 -0300

    commit..OK

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,6 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = 'Hola mundo Henryano';
+
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
 
